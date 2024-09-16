ffbf24883e1445e61e17b36a4ab545e18628661b
Frnn4268 <appstutorialesy@gmail.com>
2024-09-15 19:41:47 -0600

    Creating new component called Navbar.js and adding it's changes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,22 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <Navbar />
+      <div className="container mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
     </Router>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-500 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg font-bold">
+          <Link to="/">Personal Finance App</Link>
+        </div>
+        <div className="space-x-4">
+          <Link to="/" className="text-white hover:text-gray-200">Home</Link>
+          <Link to="/login" className="text-white hover:text-gray-200">Login</Link>
+          <Link to="/register" className="text-white hover:text-gray-200">Register</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
