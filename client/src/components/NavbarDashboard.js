bc13f28ae0e6b20d108d325f2ab5178625baac45
Frnn4268 <appstutorialesy@gmail.com>
2024-09-15 23:36:27 -0600

    Adding new styles in NavbarDashboard.js

--- client/src/components/NavbarDashboard.js
+++ client/src/components/NavbarDashboard.js
@@ -2,14 +2,23 @@ import React from 'react';
 
 const Navbar = ({ handleLogout }) => {
   return (
-    <nav className="bg-gray-800 p-4 flex justify-between items-center">
-      <div className="text-white text-lg font-bold">BudgetBuddy</div>
-      <div className="flex space-x-4">
-        <a href="/dashboard" className="text-white hover:text-yellow-500">Dashboard</a>
-        <a href="/detailed-charts" className="text-white hover:text-yellow-500">Detailed Charts</a>
-        <a href="/monthly-expenses" className="text-white hover:text-yellow-500">Monthly Expenses</a>
-        <a href="/settings" className="text-white hover:text-yellow-500">Settings</a>
-        <button onClick={handleLogout} className="bg-red-500 text-white px-4 py-2 rounded">Logout</button>
+    <nav className="bg-black p-4 h-16">
+      <div className="container mx-auto flex justify-between items-center h-full">
+        <div className="text-yellow-500 text-lg font-bold">
+          <a href="/dashboard" className="hover:text-white transition-colors duration-400">BudgetBuddy</a>
+        </div>
+        <div className="space-x-4">
+          <a href="/dashboard" className="text-white hover:text-yellow-500 transition-colors duration-300">Dashboard</a>
+          <a href="/detailed-charts" className="text-white hover:text-yellow-500 transition-colors duration-300">Detailed Charts</a>
+          <a href="/monthly-expenses" className="text-white hover:text-yellow-500 transition-colors duration-300">Monthly Expenses</a>
+          <a href="/settings" className="text-white hover:text-yellow-500 transition-colors duration-300">Settings</a>
+          <button 
+            onClick={handleLogout} 
+            className="bg-red-500 text-white font-bold px-4 py-2 rounded hover:bg-red-600 transition-colors duration-300 shadow-lg"
+          >
+            Logout
+          </button>
+        </div>
       </div>
     </nav>
   );
