e45802549b409f4e9d7b2b6734823cf7a0d27f85
Frnn4268 <appstutorialesy@gmail.com>
2024-09-15 22:34:19 -0600

    Adding new styles in Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-500 p-4 h-16">
+    <nav className="bg-black p-4 h-16">
       <div className="container mx-auto flex justify-between items-center h-full">
-        <div className="text-white text-lg font-bold">
+        <div className="text-yellow-500 text-lg font-bold">
           <Link to="/">BudgetBuddy</Link>
         </div>
         <div className="space-x-4">
-          <Link to="/" className="text-white hover:text-gray-200">Home</Link>
-          <Link to="/login" className="text-white hover:text-gray-200">Login</Link>
-          <Link to="/register" className="text-white hover:text-gray-200">Register</Link>
+          <Link to="/" className="text-white hover:text-yellow-500 transition-colors duration-300">Home</Link>
+          <Link to="/login" className="text-white hover:text-yellow-500 transition-colors duration-300">Login</Link>
+          <Link to="/register" className="text-white hover:text-yellow-500 transition-colors duration-300">Register</Link>
         </div>
       </div>
     </nav>
