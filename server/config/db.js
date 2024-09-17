6acb36fc3032032634c7e513b5b3250b1ebba1df
Frnn4268 <appstutorialesy@gmail.com>
2024-09-17 11:33:37 -0600

    Implementing logger inside of db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const logger = require('./logger'); 
 
 dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    logger.info('MongoDB connected successfully');
   } catch (err) {
-    console.error(err.message);
+    logger.error(`Error connecting to MongoDB: ${err.message}`);
     process.exit(1); 
   }
 };
